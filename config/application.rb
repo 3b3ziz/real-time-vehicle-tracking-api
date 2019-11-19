467427033402926dae48a734e811548511067a18
Ahmad Abdelaziz <a.3b3ziz@gmail.com>
2019-11-19 18:46:09 +0200

    fix: docker and local configuration

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module Door2door2backend
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
-      end if File.exists?(env_file)
+      end if File.exists?(env_file) and YAML.load(File.open(env_file))
     end
     config.api_only = true
     # config.active_job.queue_adapter = :sidekiq
